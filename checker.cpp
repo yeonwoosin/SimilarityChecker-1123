8f696d4efc84a6d332b8fc1f7803855d3b2de4d8
ywsin.kim <ywsin.kim@samsung.com>
2025-07-23 15:38:21 +0900

    add letter length checker

--- checker.cpp
+++ checker.cpp
@@ -1,19 +1,37 @@
 #include <string>
 
-class LetterNumberChecker
+using std::string;
+class LetterCountChecker
 {
 public:
-
+	explicit LetterCountChecker(const std::string& str) : refStr(str) {}
+	int run(const std::string& CheckStr)
+	{
+		int RefStrCount = refStr.size();
+		int ChkCount = CheckStr.size();
+		int B = (RefStrCount > ChkCount ? ChkCount : RefStrCount);
+		int DiffCount = (RefStrCount > ChkCount ? RefStrCount - ChkCount : ChkCount - RefStrCount) ;
+		if (B == 0) return 0;
+		int ret = (((double)1 - ((double)DiffCount / (double)B)) * MAX_VALUE);
+		return ret;
+	}
 private:
+	string refStr;
 	static const int MAX_VALUE = 60;
 };
 
 class SameLetterChecker
 {
 public:
+	explicit SameLetterChecker(const std::string& str) : refStr(str) {}
 
+	int run(const std::string& str)
+	{
+		return MAX_VALUE;
+	}
 private:
-	static const int MAX_VALUE = 60;
+	string refStr;
+	static const int MAX_VALUE = 40;
 };
 
 
@@ -22,12 +40,15 @@ class LetterChecker
 public:
 	static const int MAX_VALUE = 100;
 
-	int RunCheck(const std::string& str)
+	int RunCheck(const std::string& RefStr, const std::string& CheckStr)
 	{
-		return 100;
+		LetterCountChecker lcc(RefStr);
+		SameLetterChecker slc(RefStr);
+		int ret = 0;
+		ret += lcc.run(CheckStr);
+		ret += slc.run(CheckStr);
+		return ret;
 	}
-
 private:
-	LetterNumberChecker lnc;
-	SameLetterChecker slc;
+
 };
--- main.cpp
+++ main.cpp
@@ -4,15 +4,54 @@ using namespace testing;
 using std::string;
 
 
-TEST(TS, TC1)
+class checkerfixture : public Test
 {
+public :
 	string str = "abcdefff";
 	int num = 0;
 	LetterChecker lc;
-	num = lc.RunCheck(str);
-	EXPECT_EQ(100, num);
+};
 
+TEST_F(checkerfixture, TC_Basic)
+{
+	num = lc.RunCheck(str, "adcdefff");
+	EXPECT_EQ(100, num);
 }
+
+TEST_F(checkerfixture, TC_letter_count_zero)
+{
+	num = lc.RunCheck(str, "");
+	EXPECT_EQ(40, num);
+}
+TEST_F(checkerfixture, TC_letter_count_x2)
+{
+	num = lc.RunCheck(str, "adcdefffadcdefff");
+	EXPECT_EQ(40, num);
+}
+TEST_F(checkerfixture, TC_letter_count_part)
+{
+	num = lc.RunCheck(str, "effffff");
+	EXPECT_EQ(91, num);
+}
+
+TEST_F(checkerfixture, TC_alpha_zero)
+{
+	num = lc.RunCheck(str, "adcdefff");
+	EXPECT_EQ(100, num);
+}
+TEST_F(checkerfixture, TC_alpha_part)
+{
+	num = lc.RunCheck(str, "adcdefff");
+	EXPECT_EQ(100, num);
+}
+TEST_F(checkerfixture, TC_alpha_all)
+{
+
+	num = lc.RunCheck(str, "adcdefff");
+	EXPECT_EQ(100, num);
+}
+
+
 
 int main()
 {
